02d85ff946e254e67b764feaeed9e81572ddc2dd
LydiaLoka <sevmek87@gmail.com>
2022-11-20 14:57:25 +0300

    docs: changes in file

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 
+/* carousel */
   const owl = $('.owl-carousel');
 owl.owlCarousel( {
     center: true,
@@ -14,4 +15,15 @@ $('.slider__button--next').click(function() {
 
 $('.slider__button--prev').click(function() {
     owl.trigger('prev.owl.carousel');
-})
+});
+
+const navBtn = document.querySelector('.nav__toggle');
+const nav = document.querySelector('.nav')
+const menuIcon = document.querySelector('.menu-icon');
+
+
+navBtn.onclick = function () {
+	nav.classList.toggle('nav--mobile')
+	menuIcon.classList.toggle('menu-icon--active');
+	document.body.classList.toggle('no-scroll');
+};
